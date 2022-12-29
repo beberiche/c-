c29e3fb540bc9e5f7c418f22c623a529cf616390
beberiche <woaol@naver.com>
2022-12-29 18:30:06 +0900

    221229 cpp

--- string_method.cpp
+++ string_method.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 int main()
 {
-  string a = "";
+  string a = "love is";
+  a += " pain!";
+  a.pop_back();
+  cout << a << " : " << a.size() << "\n";
+  cout << char(*a.begin()) << "\n";
+  cout << char(*(a.end() - 1)) << "\n";
+  a.insert(0, "test ");
+  cout << a << " : " << a.size() << "\n";
+  a.erase(0, 5);
+  cout << a << " : " << a.size() << "\n";
+  auto it = a.find("love");
+  if (it != string::npos)
+  {
+    cout << "포함되어있다."
+         << "\n";
+  }
+  cout << string::npos << "\n";
+  cout << a.substr(5, 2) << "\n";
+
   return 0;
 }
